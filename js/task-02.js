43331cf447d6349a65b0ea6e1548aeb1e12f1b96
VolodymyrSalenko <vova.salenko@gmail.com>
2023-02-02 04:56:34 +0200

    Task_2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,16 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+  const listOfIngredients = ingredients.map(ingredient => {
+
+  const itemEl = document.createElement('li');
+  itemEl.textContent = ingredient;
+  itemEl.classList.add('item');
+  
+  return itemEl;
+
+});
+
+const listIngredienstEl = document.getElementById('ingredients');
+listIngredienstEl.append(...listOfIngredients);
