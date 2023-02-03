079a4ad5bb2d3cca8b717978f7eb4a165dc943f6
VolodymyrSalenko <vova.salenko@gmail.com>
2023-02-03 04:55:14 +0200

    Task9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,12 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const changeColorBtn = document.querySelector('.change-color');
+const colorValue = document.querySelector('.color');
+
+changeColorBtn.addEventListener('click', (event) => {
+
+  document.body.style.backgroundColor = getRandomHexColor();
+  colorValue.textContent = getRandomHexColor();
+});
