9619a9f2418bfc5533d42151f3cdd81c988a41cf
VolodymyrSalenko <vova.salenko@gmail.com>
2023-02-03 04:44:06 +0200

    Task8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,21 @@
 
+const formLoginEl = document.querySelector('.login-form')
+
+
+formLoginEl.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const { elements: { email, password } } = event.currentTarget;
+
+    if (email.value === "" || password.value === "") {
+        return alert("Всі поля повинні бути заповнені!");
+    };
+    
+    const userParam = {
+        email: email.value,
+        password: password.value,
+    };
+
+    console.log(userParam);
+    event.currentTarget.reset();
+});
